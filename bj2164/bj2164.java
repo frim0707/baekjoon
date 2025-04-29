929bbf433f003b6b5289b5850b82fae532ba9a25
Pepperoni pizza <frim0707@gmail.com>
2025-04-29 20:09:24 +0900

    백준 2164

---
+++ bj2164/bj2164.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class bj2164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= cnt; i++) {
+            queue.offer(i);
+        }
+
+        while(queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.peek());
+            queue.poll();
+        }
+
+        System.out.println(queue.peek());
+
+    }
+}
