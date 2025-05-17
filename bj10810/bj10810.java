160b4f44d4562c9d92b6dbba279af7f53b7c180b
Pepperoni pizza <frim0707@gmail.com>
2025-05-17 20:28:02 +0900

    백준 10810

---
+++ bj10810/bj10810.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bj10810 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int ball = Integer.parseInt(st.nextToken());
+
+            for (int j = start; j <= end; j++) {
+                basket[j] = ball;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(basket[i]).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
